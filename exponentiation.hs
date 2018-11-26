8aad81bf1479b08a73fce9c6c4c603479e88c499
Agustín Santiago Gutiérrez <elsantodel90@gmail.com>
2018-11-26 03:22:22 -0300

    Fix/emprolijado de exponentiation.hs

--- exponentiation.hs
+++ exponentiation.hs
@@ -12,7 +12,7 @@ p = 10^9 + 7
 instance Num Modu where
     (Modu a) + (Modu b) = Modu $ (a+b) `mod` p
     (Modu a) * (Modu b) = Modu $ (a*b) `mod` p
-    negate (Modu a) = Modu $ -a
+    negate (Modu a) = Modu $ (-a) `mod` p
     fromInteger = Modu . fromInteger
     abs _  = error "No sense in modular abs"
     signum = error "No sense in modular signum"
@@ -26,6 +26,6 @@ calculate [a,b] = Modu a ^ b
 -- Usando feas CString en lugar de las String nativas porque son mucho mas eficientes y sino hay TLE
 
 readInteger :: BS.ByteString -> Int
-readInteger = BS.foldl' (\x c -> 10 * x + ord c - 48) 0
+readInteger = BS.foldl' (\x c -> 10 * x + ord c - ord '0') 0
 
 main = BS.interact $ BS.unlines  . map (BS.pack .show. calculate . map readInteger . BS.words) . tail . BS.lines
